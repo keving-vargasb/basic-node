063c4b75a457c91fd8358b8a81521eb38d6542a1
Kevin Vargas <keving.vargasb@gmail.com>
2020-11-01 13:03:32 -0500

    Advance crud

--- movies-api/routes/movies.js
+++ movies-api/routes/movies.js
@@ -17,6 +17,58 @@ function moviesApi(app) {
       next(err);
     }
   });
+
+  router.get('/:movieId', async function (req, res, next) {
+    try {
+      const movies = await Promise.resolve(moviesMock[0]);
+
+      res.status(200).json({
+        data: movies,
+        message: 'Movie retrived',
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  router.post('/', async function (req, res, next) {
+    try {
+      const createMovieId = await Promise.resolve(moviesMock[0].id);
+
+      res.status(201).json({
+        data: createMovieId,
+        message: 'Movie created',
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  router.put('/:movieId', async function (req, res, next) {
+    try {
+      const updatedMovieId = await Promise.resolve(moviesMock[0].id);
+
+      res.status(200).json({
+        data: movies,
+        message: 'Movie updated',
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  router.delete('/:movieId', async function (req, res, next) {
+    try {
+      const deletedMovieId = await Promise.resolve(moviesMock[0].id);
+
+      res.status(200).json({
+        data: movies,
+        message: 'Movie deleted',
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
 }
 
 module.exports = moviesApi;
